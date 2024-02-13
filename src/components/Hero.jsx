343a22f5d9608684c3ae5db7acb24099515cb9e0
Syed Khalid <i.khalidashraf@gmail.com>
2024-02-13 09:59:16 -0500

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
 				<div>
 					<h1 className={`${styles.heroHeadText} text-white`}>
-						Hi, I am <span className='text-[#915eff]'>Syed</span>
+						Hi, I am <span className='text-[#915eff]'>Syed Khalid</span>
 					</h1>
 					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
 						I develop user interfaces and <br className='sm:block hidden' />
